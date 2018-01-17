44278d9cfd3c4ba4cbb3a56db327b7cf4d175115
Alexis Hope <alexis@cvofhope.com>
2018-01-17 12:17:36 +1100

    detect ACT from lines of play

--- app/service/PlayFormat.js
+++ app/service/PlayFormat.js
@@ -1,5 +1,10 @@
 
 class PlayFormat {
+    static ACT = /^ACT\ (I|V|X|L|C|D|M)+$/i;
+
+    isAct(text) {
+        return PlayFormat.ACT.test(text);
+    }
 }
 
 export default PlayFormat;
--- test/service/PlayFormatSpec.js
+++ test/service/PlayFormatSpec.js
@@ -4,4 +4,12 @@ describe('PlayFormat', () => {
         const playformat = new Henry.PlayFormat();
         expect(playformat).toEqual(jasmine.any(Henry.PlayFormat));
     });
+
+    it('should detect ACT', () => {
+        const playformat = new Henry.PlayFormat();
+        expect(playformat.isAct('ACT II')).toEqual(true);
+        expect(playformat.isAct('ACT XI')).toEqual(true);
+        expect(playformat.isAct('ACT MM')).toEqual(true);
+        expect(playformat.isAct('SCENE I')).toEqual(false);
+    });
 });
