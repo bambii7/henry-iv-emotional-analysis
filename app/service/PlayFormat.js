1b4d02076cd7b09733628a63411b60828bbc3128
Alexis Hope <alexis@cvofhope.com>
2018-01-17 13:05:24 +1100

    add Scene line detection

--- app/service/PlayFormat.js
+++ app/service/PlayFormat.js
@@ -2,8 +2,14 @@
 class PlayFormat {
     static ACT = /^ACT\ (I|V|X|L|C|D|M)+$/i;
 
+    static SCENE = /^SCENE\ (I|V|X|L|C|D|M)+\.(.+)/i;
+
     isAct(text) {
         return PlayFormat.ACT.test(text);
+    }
+
+    isScene(text) {
+        return PlayFormat.SCENE.test(text);
     }
 }
 
--- test/service/PlayFormatSpec.js
+++ test/service/PlayFormatSpec.js
@@ -11,5 +11,14 @@ describe('PlayFormat', () => {
         expect(playformat.isAct('ACT XI')).toEqual(true);
         expect(playformat.isAct('ACT MM')).toEqual(true);
         expect(playformat.isAct('SCENE I')).toEqual(false);
+        expect(playformat.isAct('So shaken as we are, so wan with care')).toEqual(false);
+    });
+
+    it('should detect SCENE', () => {
+        const playformat = new Henry.PlayFormat();
+        expect(playformat.isScene('SCENE I. Something')).toEqual(true);
+        expect(playformat.isScene('SCENE M. Other text')).toEqual(true);
+        expect(playformat.isScene('ACT II')).toEqual(false);
+        expect(playformat.isScene('So shaken as we are, so wan with care')).toEqual(false);
     });
 });
