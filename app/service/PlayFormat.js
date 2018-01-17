b72ce8f5e552657ed03e7db0ae4563f4c973160e
Alexis Hope <alexis@cvofhope.com>
2018-01-17 17:43:22 +1100

    structure play data in act -> scene -> content

--- app/service/PlayFormat.js
+++ app/service/PlayFormat.js
@@ -1,20 +1,35 @@
+import LineModel from '../models/LineModel';
 
 class PlayFormat {
 
-//    toStructured() {
-//        const structuredData = {};
-//        let currentAct = '';
-//        let currentScene = '';
-//        this.content.forEach((lineData) => {
-//            if (this.isAct()) {
-//                currentAct =
-//            }
-//            switch(lineData.text_entry) {
-//                case
-//            }
-//        })
-//        return this.content;
-//    }
+    static toStructured(data) {
+        const structuredData = {};
+        let currentAct = '';
+        let currentScene = '';
+        let act = '';
+        let line = '';
+        data.forEach((lineData) => {
+            const lineModel = new LineModel(lineData.text_entry);
+            const lineType = lineModel.detectType();
+            switch(lineType) {
+                case LineModel.TYPE.ACT:
+                    currentAct = lineModel.text;
+                    break;
+                case LineModel.TYPE.SCENE:
+                    currentScene = lineModel.text;
+                    break;
+                case LineModel.TYPE.CONTENT:
+                    act = structuredData[currentAct];
+                    structuredData[currentAct] = Object.assign({}, act); // initialise act if empty
+                    line = structuredData[currentAct][currentScene];
+                    line = (line === undefined ? lineModel.text : line += ' ' + lineModel.text);
+                    structuredData[currentAct][currentScene] = line;
+                    break;
+                default:
+            }
+        });
+        return structuredData;
+    }
 }
 
 export default PlayFormat;
--- test/service/PlayFormatSpec.js
+++ test/service/PlayFormatSpec.js
@@ -4,4 +4,33 @@ describe('PlayFormat', () => {
         const playFormat = new Henry.PlayFormat();
         expect(playFormat).toEqual(jasmine.any(Henry.PlayFormat));
     });
+
+    it('should structure play data into acts & scenes', () => {
+        const playData = [
+            {text_entry: 'ACT I'},
+            {text_entry: 'SCENE I. London. The palace.'},
+            {text_entry: 'Enter KING HENRY, LORD JOHN OF LANCASTER, the EARL of WESTMORELAND, SIR WALTER BLUNT, and others'},
+            {text_entry: 'SCENE II. London. An apartment of the Prince\'s'},
+            {text_entry: 'So shaken as we are, so wan with care'},
+            {text_entry: 'ACT II'},
+            {text_entry: 'SCENE I. Rochester. An inn yard.'},
+            {text_entry: 'Find we a time for frighted peace to pant'},
+            {text_entry: 'SCENE II. The highway, near Gadshill.'},
+            {text_entry: 'And breathe short-winded accents of new broils'},
+            {text_entry: 'Of hostile paces: those opposed eyes'}
+        ];
+        const actSceneData = Henry.PlayFormat.toStructured(playData);
+        expect(actSceneData).toEqual(
+            {
+                'ACT I': {
+                    'SCENE I. London. The palace.': 'Enter KING HENRY, LORD JOHN OF LANCASTER, the EARL of WESTMORELAND, SIR WALTER BLUNT, and others',
+                    'SCENE II. London. An apartment of the Prince\'s': 'So shaken as we are, so wan with care'
+                },
+                'ACT II': {
+                    'SCENE I. Rochester. An inn yard.': 'Find we a time for frighted peace to pant',
+                    'SCENE II. The highway, near Gadshill.': 'And breathe short-winded accents of new broils Of hostile paces: those opposed eyes'
+                }
+            }
+        );
+    });
 });
