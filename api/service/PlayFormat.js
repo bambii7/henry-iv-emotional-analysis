bf4be68acdbfe396b8cf39ce1fc64e03ddec1f3d
Alexis Hope <alexis@cvofhope.com>
2018-01-20 07:46:28 +1100

    improve readiblity of PlayFormatter

--- api/service/PlayFormat.js
+++ api/service/PlayFormat.js
@@ -6,8 +6,6 @@ class PlayFormat {
         const structuredData = {};
         let currentAct = '';
         let currentScene = '';
-        let act = '';
-        let line = '';
         data.forEach((lineData) => {
             const lineModel = new LineModel(lineData.text_entry);
             const lineType = lineModel.detectType();
@@ -19,11 +17,12 @@ class PlayFormat {
                     currentScene = lineModel.text;
                     break;
                 case LineModel.TYPE.CONTENT:
-                    act = structuredData[currentAct];
-                    structuredData[currentAct] = Object.assign({}, act); // initialise act if empty
-                    line = structuredData[currentAct][currentScene];
-                    line = (line === undefined ? lineModel.text : line += ' ' + lineModel.text);
-                    structuredData[currentAct][currentScene] = line;
+                    if (structuredData[currentAct]) {
+                        structuredData[currentAct] = {
+                            [currentScene]: ''
+                        };
+                    }
+                    structuredData[currentAct][currentScene] += ' ' + lineModel.text;
                     break;
                 default:
             }
